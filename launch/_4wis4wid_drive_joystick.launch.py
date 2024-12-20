8dc7897eb4152cea7d1b6fcc889c14c258465b38
albic98 <albert.androsic2@gmail.com>
2024-12-20 14:40:38 +0100

    Update of launch files

--- launch/_4wis4wid_drive_joystick.launch.py
+++ launch/_4wis4wid_drive_joystick.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
         executable='teleop_node',
         name='teleop_node',
         parameters=[joy_params],
-        remappings=[('/fcc/cmd_vel','/cmd_vel_joy')]
+        remappings=[('/cmd_vel','/cmd_vel_joy')]
     )
 
     odometry_node = Node(
